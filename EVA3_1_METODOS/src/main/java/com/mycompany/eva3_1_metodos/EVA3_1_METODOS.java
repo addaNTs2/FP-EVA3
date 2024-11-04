2e61f50b6f8da0c647a2cd9ed90ea753a993d0d6
addaNTs2 <L24550283@chihuahua2.tecnm.mx>
2024-11-04 17:52:23 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/main/java/com/mycompany/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_metodos;
+
+/**
+ *
+ * @author Angel
+ */
+public class EVA3_1_METODOS {
+    public static void main(String[] args) {
+        //USO DE LOS METODOS:
+        //INVOCACION O LLAMADA
+        imprimirMensaje("Hola mundo");
+    }
+    //DECLARACION
+    //header (encabezado) para crear metodos
+    //imprimir un mensaje
+    //JAVA: iniciar en minuscula, verbo, siguientes palabras Inicin en mayusculas
+    public static void imprimirMensaje(String mensaje ){
+        System.out.println("Tu mensaje: " + mensaje);}
+}
